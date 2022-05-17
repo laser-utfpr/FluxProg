00d9c271962d4a169e22d947bde66791183f7ce7
Fernando <fernandoratusznei@hotmail.com>
2022-05-17 15:10:13 -0300

    test motor control on line

--- carrinho_robo_educ/carrinho_robo_educ.ino
+++ carrinho_robo_educ/carrinho_robo_educ.ino
@@ -7,9 +7,67 @@ const int pin_ir[4] = { 45, 47, 49, 51 };
 int error_table[16];
 //*****************************************************************
 
+struct MotorController {
+  static constexpr int resolucao = 20; // lacunas por volta
+  static constexpr float circunferencia = 22; // 7pi cm, a roda tem D = 7cm
+  static constexpr float kp = 0.8;
+  static constexpr float ki = 0;
+  static constexpr float kd = 0.1;
 
-AF_DCMotor m1(1);
-AF_DCMotor m2(2);
+  AF_DCMotor motor;
+  int motor_power = 0;
+
+  int counter = 0;
+  int last_counter = 0;
+  unsigned long last_time = 0;
+  unsigned long now = 0;
+  float dt;
+
+  int err = 0;
+  int last_err = 0;
+  float speed;
+  float target = 0;
+
+  MotorController(int id): motor(id) {}
+
+  float get_cm_s (int counter, float dt) {
+    return (1000 * circunferencia / resolucao) * (counter / dt);
+  }
+
+  float get_rad_s (int counter, float dt) {
+    return (1000 / resolucao) * (counter / dt);
+  }
+
+  float get_rpm (int counter, float dt) {
+    return 60 * (1000 / resolucao) * (counter / dt);
+  }
+
+  void run(int dir) { motor.run(dir); }
+  void setSpeed(float speed) { target = speed; }
+
+  float get_speed() {
+    now = millis();
+    dt = now - last_time;
+    last_time = now;
+
+    last_counter = counter;
+    counter = 0;
+    return get_rad_s(last_counter, dt);
+  }
+
+  void pid() {
+    speed = get_speed();
+    err = target - speed;
+
+    motor_power += err*kp + kd*(err-last_err);
+    motor_power = min(255, max(motor_power, 0));
+    motor.setSpeed(motor_power);
+    last_err = err;
+  }
+};
+
+MotorController m1(1);
+MotorController m2(2);
 
 //Funções----------------------------------------------------------
 // motor
@@ -87,6 +145,8 @@ void follow_line() {
 void setup() {
   // Serial.begin(9600);
   init_ir();
+  attachInterrupt(digitalPinToInterrupt(20), increment1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(21), increment2, FALLING);
 }
 
 
